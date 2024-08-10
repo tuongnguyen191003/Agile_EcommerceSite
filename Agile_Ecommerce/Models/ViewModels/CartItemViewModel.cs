75035208e6a4de3e6f365296918fc99d28e17e92
lephuctai <tai1672003@gmail.com>
2024-08-10 19:19:45 +0700

    Tài - Thêm Chức năng trang giỏ hàng cho người dùng và tạo thông báo

---
+++ Agile_Ecommerce/Models/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Agile_Ecommerce.Models;
+
+namespace ShoppingOnline.Models.ViewModels
+{
+	public class CartItemViewModel
+	{
+		public List<CartItemModel> CartItems { get; set; }
+		public decimal GrandTotal { get; set; }
+	}
+}
---
+++ Agile_Ecommerce/Models/ViewModels/CategoryProductsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Agile_Ecommerce.Models;
+using ShoppingOnline.Models;
+
+namespace ShoppingOnline.Models.ViewModels
+{
+	public class CategoryProductsViewModel
+	{
+		public CategoryModel Category { get; set; }
+		public List<ProductViewModel> Products { get; set; }
+	}
+}
