df99b98a74a6ceb25d6ac7e2aa5c409a5e7e3a60
nguyenminhtuong <tuongnguyen191003@gmail.com>
2024-08-10 16:27:21 +0700

    Tường - Thêm Chức năng sản phẩm cho trang người dùng

--- Agile_Ecommerce/Controllers/ProductController.cs
+++ Agile_Ecommerce/Controllers/ProductController.cs
@@ -1,17 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Agile_Ecommerce.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Agile_Ecommerce.Controllers
 {
 	public class ProductController : Controller
 	{
+		private readonly DataContext _dataContext;
+		public ProductController(DataContext context)
+		{
+			_dataContext = context;
+		}
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult Details()
+		public async Task<IActionResult> Details(int Id)
 		{
-			return View();
+			if (Id == null) return RedirectToAction("Details");
+			var productsById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault();
+			return View(productsById);
 		}
 	}
 }
